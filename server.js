42a85dedfbf0249b0982a6fe1743f36348aeab16
sskyiz <85129059+sskyiz@users.noreply.github.com>
2021-06-12 01:29:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
-// Cors 
+ 
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
-
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
 
 app.use(cors(corsOptions))
 app.use(express.static('public'));
@@ -31,7 +21,7 @@ app.set('view engine', 'ejs');
 
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
-app.use('/files/download', require('./routes/download'));
+app.use('files/download', require('./routes/download'));
 
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
