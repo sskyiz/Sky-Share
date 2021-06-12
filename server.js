a35505a6d447a50bda00b0e6c2d207fcfa5b458d
sskyiz <85129059+sskyiz@users.noreply.github.com>
2021-06-12 20:36:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use('/api/files', require('./routes/files'));
-app.use('/files', require('./routes/show'));
+app.use('files', require('./routes/show'));
 app.use('files/download', require('./routes/download'));
 
 
