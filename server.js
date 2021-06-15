93b8da4eaf10cc8e615d60b3d20fd6c01c959aeb
sskyiz <85129059+sskyiz@users.noreply.github.com>
2021-06-15 10:51:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
-app.use('files/download', require('./routes/download'));
+app.use('/files/download', require('./routes/download'));
 
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
